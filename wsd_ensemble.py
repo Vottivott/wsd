589be70497ade3dfc42078442a5bdf70ae2bc585
Hannes von Essen <hannes.von.essen@gmail.com>
2019-11-15 00:56:00 +0100

    Update

--- wsd_ensemble.py
+++ wsd_ensemble.py
@@ -1,6 +1,6 @@
 from wsd import wsd
 
-wsd(model_name='ensemble-distil-2-albert-2')
+#wsd(model_name='ensemble-distil-2-albert-2')
 
-wsd(model_name='ensemble-distil-2-albert-2', classifier_hidden_layers=[768])
 wsd(model_name='ensemble-distil-1-albert-1', classifier_hidden_layers=[768])
+wsd(model_name='ensemble-distil-2-albert-2', classifier_hidden_layers=[768])
